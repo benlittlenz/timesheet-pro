6214d4728591cc9add2ad1bf6e023457ddaa2e7b
benlittlenz <benlittle53@gmail.com>
2021-04-04 10:24:36 +1200

    added model relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Timesheet;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function timesheet()
+    {
+        return $this->hasMany(Timesheet::class);
+    }
 }
