71c96908fc0740e6930df42f1b7ccc15bf459399
benlittlenz <benlittle53@gmail.com>
2021-04-04 10:36:52 +1200

    added job factory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -22,7 +22,11 @@ class JobFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'job_ref' => $this->faker->randomNumber(2) . $this->faker->word(),
+            'job_description' => $this->faker->paragraph(3),
+            'address' => $this->faker->streetAddress,
+            'suburb' => $this->faker->state,
+            'city' => $this->faker->city,
         ];
     }
 }
